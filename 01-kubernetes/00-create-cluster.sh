8cf7540e13a32faaf8e5a4b377a030780c90b5cf
Marco Alarcón <marco.alarcon.s@gmail.com>
2023-04-05 17:43:56 -0400

    Primer commit

--- 01-kubernetes/00-create-cluster.sh
+++ 01-kubernetes/00-create-cluster.sh
@@ -2,4 +2,4 @@
 mkdir -p /tmp/k3dvol
 
 k3d cluster create dev --servers 1 --agents 2 --k3s-arg "--disable=traefik@server:*" --volume /tmp/k3dvol:/tmp/k3dvol -p "8081:80@loadbalancer"
-export KUBECONFIG="$(k3d get-kubeconfig --name='k3d-cluster')"
+#export KUBECONFIG="$(k3d get-kubeconfig --name='k3d-cluster')"
