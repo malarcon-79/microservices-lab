f5f99b41124b430dc586ceb08b085a0110a69b6e
Marco Alarcón <marco.alarcon.s@gmail.com>
2023-04-12 16:42:53 -0400

    Primer commit

--- orm-go/model/invoiceItem.go
+++ orm-go/model/invoiceItem.go
@@ -6,7 +6,7 @@ import (
 
 type InvoiceItem struct {
 	ID        int32           `gorm:"primary_key;column:id;type:INT4 NOT NULL AUTO_INCREMENT" json:"id"`
-	InvoiceId int32           `gorm:"column:invoice_id;type:TEXT NOT NULL;" json:"invoice_id"`
+	InvoiceId int32           `gorm:"column:invoice_id;type:INT4 NOT NULL;" json:"invoice_id"`
 	Details   string          `gorm:"column:details;type:TEXT NOT NULL" json:"details"`
 	Amount    decimal.Decimal `gorm:"column:amount;type:decimal(20, 8) NOT NULL;" json:"amount"`
 }
